3ab417e8d901877c6ed5cb13c943398f660da062
LixinSy <1696662197@qq.com>
2021-12-31 12:11:03 +0800

    完善Time

--- core/core_time.cpp
+++ core/core_time.cpp
@@ -1,6 +1,28 @@
 #include "core_time.h"
 #include <cstring>
 
+void get_localtime(const time_t *t, struct tm *tmout) {
+#if __POSIX__
+    ::localtime_r(t, tmout);
+#elif __WINDOWS__
+    ::localtime_s(tmout, t);
+#else
+    struct tm *tv = std::localtime(t);
+    memcpy((void*)tmout, (const void*)tv, sizeof (struct tm));
+#endif
+}
+
+void get_gmtime(const time_t *t, struct tm *tmout) {
+#if __POSIX__
+    ::gmtime_r(t, tmout);
+#elif __WINDOWS__
+    ::gmtime_s(tmout, t);
+#else
+    struct tm *tv = std::gmtime(t);
+    memcpy((void*)tmout, (const void*)tv, sizeof (struct tm));
+#endif
+}
+
 Time Time::parser(std::string date_time) {
     Time date;
     std::string format;
@@ -29,11 +51,13 @@ time_t Time::utc() {
 int Time::timeoffset() {
     time_t t = std::time(NULL);
     //获取utc时间
-    struct tm gtm = *std::gmtime(&t);
+    struct tm gtm;
+    get_gmtime(&t, &gtm);
     //mktime参数含义为localtime, 返回localtime对应的utc时间戳。
     //这里传入gmtime, 返回t的偏移时间戳
     time_t gt = std::mktime(&gtm);
-    struct tm gtm2 = *std::localtime(&gt);
+    struct tm gtm2;
+    get_localtime(&gt, &gtm2);
     int offset = ((t - gt ) + (gtm2.tm_isdst > 0 ? 3600 : 0));
     return offset;
 }
@@ -45,15 +69,13 @@ int Time::timezone() {
 Time::Time() {
     timestamp_ = std::time(nullptr);
     tm_val_ = new struct tm();
-    struct tm *tv = std::localtime(&timestamp_);
-    memcpy((void*)tm_val_, (const void*)tv, sizeof (struct tm));
+    get_localtime(&timestamp_, tm_val_);
 }
 
 Time::Time(time_t timestump) {
     timestamp_ = timestump;
     tm_val_ = new struct tm();
-    struct tm *tv = std::localtime(&timestamp_);
-    memcpy((void*)tm_val_, (const void*)tv, sizeof (struct tm));
+    get_localtime(&timestamp_, tm_val_);
 }
 
 Time::Time(const Time &other) {
@@ -62,38 +84,85 @@ Time::Time(const Time &other) {
     memcpy((void*)tm_val_, (const void*)other.tm_val_, sizeof (struct tm));
 }
 
+Time::Time(int year, int month, int day, int hour, int minute, int second) {
+    tm_val_ = new struct tm();
+    tm_val_->tm_year = year-1900;
+    tm_val_->tm_mon = month-1;
+    tm_val_->tm_mday = day;
+    tm_val_->tm_hour = hour;
+    tm_val_->tm_min = minute;
+    tm_val_->tm_sec = second;
+    timestamp_ = std::mktime(tm_val_);
+}
+
 Time::~Time() {
     delete tm_val_;
 }
 
-std::string Time::format(Time::Format format) const {
-    std::string str = "";
-    char tmpbuf[32] = { 0 };
+std::string Time::format(TimeFormat format) const {
+    std::string str;
+    size_t n = 0;
+    str.resize(32);
     switch (format) {
-    case Time::Format::YMDHMS_S:
-        std::strftime(tmpbuf, sizeof (tmpbuf), "%Y-%m-%d %H:%M:%S", tm_val_);
-        str = std::string(tmpbuf);
+    case TimeFormat::YMDHMS_S:
+        n = std::strftime(&str[0], str.capacity(), "%Y-%m-%d %H:%M:%S", tm_val_);
         break;
-    case Time::Format::YMDHMS_N:
-        std::strftime(tmpbuf, sizeof (tmpbuf), "%Y%m%d%H%M%S", tm_val_);
-        str = std::string(tmpbuf);
+    case TimeFormat::YMDHMS_N:
+        n = std::strftime(&str[0], str.capacity(), "%Y%m%d%H%M%S", tm_val_);
         break;
-    case Time::Format::YMD_S:
-        std::strftime(tmpbuf, sizeof (tmpbuf), "%Y-%m-%d", tm_val_);
-        str = std::string(tmpbuf);
+    case TimeFormat::YMD_S:
+        n = std::strftime(&str[0], str.capacity(), "%Y-%m-%d", tm_val_);
         break;
-    case Time::Format::YMD_N:
-        std::strftime(tmpbuf, sizeof (tmpbuf), "%Y%m%d", tm_val_);
-        str = std::string(tmpbuf);
+    case TimeFormat::YMD_N:
+        n = std::strftime(&str[0], str.capacity(), "%Y%m%d", tm_val_);
         break;
-    case Time::Format::HMS_S:
-        std::strftime(tmpbuf, sizeof (tmpbuf), "%H:%M:%S", tm_val_);
-        str = std::string(tmpbuf);
+    case TimeFormat::HMS_S:
+        n = std::strftime(&str[0], str.capacity(), "%H:%M:%S", tm_val_);
         break;
-    case Time::Format::HMS_N:
-        std::strftime(tmpbuf, sizeof (tmpbuf), "%H%M%S", tm_val_);
-        str = std::string(tmpbuf);
+    case TimeFormat::HMS_N:
+        n = std::strftime(&str[0], str.capacity(), "%H%M%S", tm_val_);
         break;
     }
+    str.resize(n);
     return str;
+}
+
+Time& Time::operator += (const time_t& t) {
+    timestamp_ += t;
+    get_localtime(&timestamp_, tm_val_);
+    return *this;
+}
+
+Time& Time::operator -= (const time_t& t) {
+    timestamp_ -= t;
+    get_localtime(&timestamp_, tm_val_);
+    return *this;
+}
+
+time_t Time::operator - (const Time &other) const {
+    return timestamp_ - other.timestamp_;
+}
+
+bool Time::operator < (const Time &other) const {
+    return timestamp_ < other.timestamp_;
+}
+
+bool Time::operator <= (const Time &other) const {
+    return timestamp_ <= other.timestamp_;
+}
+
+bool Time::operator > (const Time &other) const {
+    return timestamp_ > other.timestamp_;
+}
+
+bool Time::operator >= (const Time &other) const {
+    return timestamp_ >= other.timestamp_;
+}
+
+bool Time::operator == (const Time &other) const {
+    return timestamp_ == other.timestamp_;
+}
+
+bool Time::operator != (const Time &other) const {
+    return timestamp_ != other.timestamp_;
 }
--- core/core_time.h
+++ core/core_time.h
@@ -6,48 +6,48 @@
 #include "def.h"
 
 
+enum class TimeFormat {
+    YMDHMS_S = 0,   //2006-01-02 15:04:05
+    YMDHMS_N,       //20060102150405
+    YMD_S,          //2006-01-02
+    YMD_N,          //20060102
+    HMS_S,          //15:04:05
+    HMS_N,          //150405
+};
+
+enum class Week {
+    SUNDAY = 0,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+};
+
+enum class Month {
+    JANUARY = 1,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER,
+};
+
 class Time final
 {
 public:
-    enum  {
-        SECOND = 1,
-        MINUTE_S = 60,
-        HOUR_S = 60 * MINUTE_S,
-        ONE_DAY_S = 24 * HOUR_S,
-        ONE_WEEK_S = 7 * ONE_DAY_S,
-    };
-    enum Format {
-        YMDHMS_S = 0,   //2006-01-02 15:04:05
-        YMDHMS_N,       //20060102150405
-        YMD_S,          //2006-01-02
-        YMD_N,          //20060102
-        HMS_S,          //15:04:05
-        HMS_N,          //150405
-    };
-    enum Week {
-        SUNDAY = 0,
-        MONDAY,
-        TUESDAY,
-        WEDNESDAY,
-        THURSDAY,
-        FRIDAY,
-        SATURDAY,
-    };
-    enum Month {
-        JANUARY = 1,
-        FEBRUARY,
-        MARCH,
-        APRIL,
-        MAY,
-        JUNE,
-        JULY,
-        AUGUST,
-        SEPTEMBER,
-        OCTOBER,
-        NOVEMBER,
-        DECEMBER,
-    };
-public:
+    static const int SECOND = 1;
+    static const int MINUTE_S = 60;
+    static const int HOUR_S = 60 * MINUTE_S;
+    static const int ONE_DAY_S = 24 * HOUR_S;
+    static const int ONE_WEEK_S = 7 * ONE_DAY_S;
     static Time parser(std::string date_time);
     static Time now();
     //返回当前UTC时间戳
@@ -57,6 +57,7 @@ public:
 public:
     Time();
     Time(time_t timestump);
+    Time(int year, int month, int day, int hour, int minute, int second);
     Time(const Time &other);
     ~Time();
 
@@ -71,10 +72,23 @@ public:
     int isdist() const { return tm_val_->tm_isdst; }
 
     time_t timestamp() const { return timestamp_; }
-    std::string format(Time::Format format) const;
+    std::string format(TimeFormat format) const;
+
+    Time& operator += (const time_t& t);
+    Time& operator -= (const time_t& t);
+    time_t operator - (const Time &other) const;
+    bool operator < (const Time &other) const;
+    bool operator <= (const Time &other) const;
+    bool operator > (const Time &other) const;
+    bool operator >= (const Time &other) const;
+    bool operator == (const Time &other) const;
+    bool operator != (const Time &other) const;
 private:
     time_t timestamp_;      //UTC时间戳
     struct tm *tm_val_;
 };
+
+void get_localtime(const time_t *t, struct tm *tmout);
+void get_gmtime(const time_t *t, struct tm *tmout);
 
 #endif // TIME_H
