8c2a9db007c508ddeb83d7b2184f93657ec6356a
LixinSy <1696662197@qq.com>
2022-01-04 14:55:09 +0800

    恢复tcp_server

--- net/net_tcp_server.h
+++ net/net_tcp_server.h
@@ -1,11 +1,26 @@
 #ifndef NET_TCP_SERVER_H
 #define NET_TCP_SERVER_H
 
+#include "net_event_module.h"
 
-class net_tcp_server
+class Socket;
+class ParserCreator;
+
+class TcpServer: NetEvent
 {
 public:
-    net_tcp_server();
+    TcpServer(const std::string &host, uint16 port, uint16 backlog, ParserCreator *creator, EventModule *module);
+    virtual ~TcpServer();
+    virtual int handle_input() override;
+    virtual int handle_output() override;
+    virtual int handle_close() override;
+    bool start();
+private:
+    std::string host_;
+    uint16 port_;
+    uint16 backlog_;
+    Socket *sock_;
+    ParserCreator *parser_creator_;
 };
 
 #endif // NET_TCP_SERVER_H
