7966715c321e70a0960364bddb3759df9ceadda8
LixinSy <1696662197@qq.com>
2022-01-04 14:48:18 +0800

    完善time util

--- common/comm_time_util.h
+++ common/comm_time_util.h
@@ -3,16 +3,51 @@
 
 #include "def.h"
 #include <sys/time.h>
-
-#define ONE_HOUR_S 60*60
-#define ONE_DAY_S 24*60*60
+#include "core_time.h"
 
 struct TimeUtil
 {
-    static uint64 get_millisecond(){
+    //获取毫秒
+    static uint64 get_millisecond() {
         struct timeval tv;
         gettimeofday(&tv, nullptr);
         return tv.tv_sec * 1000ULL + tv.tv_usec / 1000ULL;
+    }
+
+    //是否跨天某一小时, hour(0-23)
+    static bool is_new_day_reset(time_t before, time_t after,  int resetHour) {
+        before += Time::timeoffset();
+        after += Time::timeoffset();
+        return ((before - resetHour * Time::HOUR_S) / Time::ONE_DAY_S) != ((after - resetHour * Time::HOUR_S) / Time::ONE_DAY_S);
+    }
+
+    //获取下次重置时间（时刻）, nowSec为UTC时间戳
+    static time_t GetNextResetTime(time_t nowSec, int resetHour) {
+        time_t thisDayResetTime = nowSec - ((nowSec + Time::timeoffset()) % Time::ONE_DAY_S) + (int64(resetHour) * Time::HOUR_S);
+        if (thisDayResetTime > nowSec) {
+            return thisDayResetTime;
+        } else {
+            return thisDayResetTime + Time::ONE_DAY_S;
+        }
+    }
+
+    //是否跨周before, after为UTC时间戳
+    static bool IsWeekReset(time_t before, time_t after, int resetHour, int resetWeekDay) {
+        time_t weekResetTime = TimeUtil::GetNextWeekResetTime(before, resetHour, resetWeekDay);
+        return after > weekResetTime;
+    }
+
+    //获取下周重置时间（时刻）, nowSec为UTC时间戳
+    static time_t GetNextWeekResetTime(time_t nowSec, int resetHour, int resetWeekDay) {
+        Time nowTime(nowSec);
+        int nowWeekDay = nowTime.wday();
+        time_t diff = time_t(resetWeekDay-nowWeekDay) * Time::ONE_DAY_S;
+        time_t thisWeekResetTime = nowSec - ((nowSec + Time::timeoffset()) % Time::ONE_DAY_S) + diff + (int64(resetHour) * Time::HOUR_S);
+        if (thisWeekResetTime > nowSec) {
+            return thisWeekResetTime;
+        } else {
+            return thisWeekResetTime + Time::ONE_WEEK_S;
+        }
     }
 
 };
--- net/net_tcp_server.h
+++ net/net_tcp_server.h
@@ -1,26 +1,11 @@
 #ifndef NET_TCP_SERVER_H
 #define NET_TCP_SERVER_H
 
-#include "net_event_module.h"
 
-class Socket;
-class ParserCreator;
-
-class TcpServer: NetEvent
+class net_tcp_server
 {
 public:
-    TcpServer(const std::string &host, uint16 port, uint16 backlog, ParserCreator *creator, EventModule *module);
-    virtual ~TcpServer();
-    virtual int handle_input() override;
-    virtual int handle_output() override;
-    virtual int handle_close() override;
-    bool start();
-private:
-    std::string host_;
-    uint16 port_;
-    uint16 backlog_;
-    Socket *sock_;
-    ParserCreator *parser_creator_;
+    net_tcp_server();
 };
 
 #endif // NET_TCP_SERVER_H
