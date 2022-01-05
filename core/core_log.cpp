8516d8f234c257b6e11552130e4a6cc66d423fba
LixinSy <1696662197@qq.com>
2022-01-05 11:41:36 +0800

    log

--- core/core_log.cpp
+++ core/core_log.cpp
@@ -1,20 +1,47 @@
 #include "core_log.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <filesystem>
 #include "comm_time_util.h"
 #include "comm_str_util.h"
 #include "core_mutex.h"
 #include "core_byte_buffer.h"
+#include "core_time.h"
 
-#define MAX_BUFF_SIZE 1024
+namespace stdfs = std::filesystem;
 
+#define MAX_BUFF_SIZE 10*1024*1024
+#define LINE_BUFF_SIZE 10*1024
+
+std::string get_log_level_name(uint8 level) {
+    std::string str = " ";
+    switch (level) {
+    case LogLevel::LOG_FATAL:
+        str.append("FATAL"); break;
+    case LogLevel::LOG_ERROR:
+        str.append("ERROR"); break;
+    case LogLevel::LOG_WARN:
+        str.append("WARN"); break;
+    case LogLevel::LOG_INFO:
+        str.append("INFO"); break;
+    case LogLevel::LOG_DEBUG:
+        str.append("DEBUG"); break;
+    case LogLevel::LOG_TRACE:
+        str.append("TRACE"); break;
+    }
+    str.append(" ");
+    return str;
+}
 
 XLog::XLog() {
     fd_ = nullptr;
-    lines_ = 0;
+    file_size_ = 0;
+    index_ = 0;
     mtx_ = new Mutex(false);
-    data_.reserve(MAX_BUFF_SIZE*1024);
+    data_.reserve(MAX_BUFF_SIZE);
 }
+
 XLog::~XLog() {
     if (fd_) {
         fclose(fd_);
@@ -25,10 +52,6 @@ XLog::~XLog() {
 bool XLog::init(const LogParam &param) {
     param_ = param;
     return open_new_file();
-}
-
-void XLog::set_prefix(std::string name) {
-    param_.prefix = name;
 }
 
 void XLog::set_level(uint8 level) {
@@ -48,65 +71,109 @@ bool XLog::open_new_file() {
         ::fclose(fd_);
         fd_ = nullptr;
     }
-    std::string file_name = "./log/" + param_.prefix + "_" +std::to_string(TimeUtil::get_millisecond()) + ".log";
-    fd_ = fopen(file_name.c_str(), "w");
+    std::string date = Time().format(TimeFormat::YMD_N);
+    std::string file_name = param_.prefix + date;
+    if (index_ > 0) {
+        file_name.append(".");
+        file_name.append(std::to_string(index_));
+    }
+    file_name.append(".log");
+    stdfs::path dpath(param_.out_path);
+    if (!stdfs::exists(dpath)) {
+        if (!stdfs::create_directories(dpath)) {
+            fprintf(stderr, "create_directories %s err\n", dpath.string().c_str());
+            return false;
+        }
+    }
+    dpath /= stdfs::path(file_name);
+    fd_ = ::fopen(dpath.string().c_str(), "a");
     if (!fd_) {
-        fprintf(stderr, "%s fopen err\n", file_name.c_str());
+        fprintf(stderr, "fopen %s err\n", dpath.string().c_str());
         return false;
     } else {
+        fprintf(stderr, "create new log %s ok\n", dpath.string().c_str());
         return true;
     }
 }
 
-void XLog::xprintf(uint8 level, const char cszFormat[], ...) {
+void XLog::xprintf(uint8 level, const char *func, int line, const char cszFormat[], ...) {
     if (level > param_.level) {
         return;
     }
-    char buf[MAX_BUFF_SIZE];
+    std::string prefix;
+    prefix.append(Time().format(TimeFormat::YMDHMS_S));
+    prefix.append(get_log_level_name(level));
+    prefix.append("[");
+    prefix.append(func, strlen(func));
+    prefix.append(std::to_string(line));
+    prefix.append("] ");
+    static char line_buf[LINE_BUFF_SIZE];
     va_list ap;
     va_start(ap, cszFormat);
-    int len = ::vsnprintf(buf, MAX_BUFF_SIZE, cszFormat, ap);
+    int len = ::vsnprintf(line_buf, LINE_BUFF_SIZE-1, cszFormat, ap);
     va_end(ap);
     if (len > 0) {
+        line_buf[len] = '\n';
         mtx_->lock();
-        ++lines_;
-        data_.append(buf, len);
+        data_.append(prefix);
+        data_.append(line_buf, len+1);
         mtx_->unlock();
     }
 }
 
 void XLog::run() {
-    while (wait_for(2000)) {
-        printf("log run\n");
-        //ttl log ...
+    time_t now_sec = Time::utc();
+    int next = TimeUtil::get_next_reset_time(now_sec, 0);
+    int dur = (next - now_sec) * 1000;
+    start_timer(dur, false);
+    while (wait_for(1000)) {
         mtx_->lock();
         if (data_.size() <= 0) {
             mtx_->unlock();
             continue;
         }
         const char *data = data_.c_str();
-        if (param_.option & LogOption::XLOG_OPTION_FILE) {
+        if (param_.option & LogOption::LOG_OPTION_FILE) {
             if  (!fd_) {
-                open_new_file();
+                if (!open_new_file()) {
+                    ::fprintf(stderr, "%s", "XLog open file err");
+                }
             }
             if (fd_) {
                 uint32 put_len = 0;
                 for (int len; put_len < data_.size(); put_len += len) {
                     len = ::fprintf(fd_, "%s", data+put_len);
                 }
-                fflush(fd_);
-                if (lines_ >= param_.max_line) {
-                    open_new_file();
+                ::fflush(fd_);
+                file_size_ += data_.size();
+                if (file_size_ >= param_.max_size) {
+                    index_++;
+                    if (!open_new_file()) {
+                        ::fprintf(stderr, "%s", "XLog open file err");
+                    }
                 }
             }
         }
-        if (param_.option & LogOption::XLOG_OPTION_STDOUT) {
+        if (param_.option & LogOption::LOG_OPTION_STDOUT) {
             ::fprintf(stdout, "%s", data);
-        }
-        if (param_.option & LogOption::XLOG_OPTION_STDERR) {
-            ::fprintf(stderr, "%s", data);
         }
         data_.clear();
         mtx_->unlock();
+    }
+}
+
+void XLog::on_timer() {
+    //跨天重新生成文件
+    index_ = 0;
+    mtx_->lock();
+    bool ret = open_new_file();
+    mtx_->unlock();
+    if (!ret) {
+        ::fprintf(stderr, "%s", "XLog on_timer open file err");
+    } else {
+        time_t now_sec = Time::utc();
+        int next = TimeUtil::get_next_reset_time(now_sec, 0);
+        int dur = (next - now_sec) * 1000;
+        start_timer(dur, false);
     }
 }
--- core/core_log.h
+++ core/core_log.h
@@ -3,44 +3,45 @@
 
 #include "def.h"
 #include "core_thread.h"
-
+#include "core_timer.h"
 
 // 日志输出级别
 enum LogLevel: uint8
 {
-    XLOG_NONE		=	0,	// 不打印日志
-    XLOG_CRASH		=   1,  // 严重错误,导致程序不能继续运行
-    XLOG_ERR		=   2,  // 程序错误,导致处理失败(如申请内存失败等)
-    XLOG_WARNING	=	3,	// 程序警告,可能导致错误产生(如传入不合法参数)
-    XLOG_NOTICE		=   4,  // 正常但是值得注意的情况
-    XLOG_INFO	    =   5,  // 业务相关的信息(不影响程序流程，如显示用户登陆下线信息)
-    XLOG_DEBUG	    =   6,  // 调试信息(不影响程序流程，如打印当前内存池中未使用内存块数目等)
-    XLOG_TRACE		=	7,	// 打印程序运行轨迹
-    XLOG_LEVEL_LIMIT
+    LOG_NONE		=	0,	// 不打印日志
+    LOG_FATAL		=   1,  // 严重错误,导致程序不能继续运行
+    LOG_ERROR       =   2,  // 程序错误,导致处理失败(如申请内存失败等)
+    LOG_WARN        =	3,	// 程序警告,可能导致错误产生(如传入不合法参数)
+    LOG_NOTICE		=   4,  // 正常但是值得注意的情况
+    LOG_INFO	    =   5,  // 业务相关的信息(不影响程序流程，如显示用户登陆下线信息)
+    LOG_DEBUG	    =   6,  // 调试信息(不影响程序流程，如打印当前内存池中未使用内存块数目等)
+    LOG_TRACE		=	7,	// 打印程序运行轨迹
 };
 
 // 日志输出位置
 enum LogOption: uint8
 {
-    XLOG_OPTION_FILE		=   0x01,   // log on to file, default
-    XLOG_OPTION_STDOUT		=   0x02,   // log on the console if errors in sending
-    XLOG_OPTION_STDERR		=   0x04,   // log on the stderr stream
-    XLOG_OPTION_TCP			=   0x08,	// log on the tcp stream
+    LOG_OPTION_FILE         =   0x01,   // log on to file, default
+    LOG_OPTION_STDOUT		=   0x02,   // log on the console if errors in sending
+    //LOG_OPTION_STDERR		=   0x04,   // log on the stderr stream
+    //LOG_OPTION_TCP			=   0x08,	// log on the tcp stream
 };
 
 struct LogParam
 {
+    std::string out_path;
     std::string prefix;
     uint8 level;
     uint8 option;
-    uint32 max_line;
+    uint32 max_size;
     uint32 keep_days;
 
     LogParam()
-        : prefix("log"),
-          level(LogLevel::XLOG_TRACE),
-          option(LogOption::XLOG_OPTION_FILE),
-          max_line(1000),
+        : out_path("./log/"),
+          prefix("game_"),
+          level(LogLevel::LOG_TRACE),
+          option(LogOption::LOG_OPTION_FILE),
+          max_size(100*1024*1024), //100M
           keep_days(30)
     {
     }
@@ -51,7 +52,7 @@ struct LogParam
 class ByteBuffer;
 class Mutex;
 
-class XLog: public Runable
+class XLog: public Runable, public Timer
 {
 public:
     static XLog *instance() {
@@ -59,32 +60,31 @@ public:
         return &obj;
     }
     bool init(const LogParam &param);
-    void set_prefix(std::string name);
     void set_level(uint8 level);
-    void set_option(uint8);
+    void set_option(uint8 option);
     void set_keep_days(uint32 days);
-    bool open_new_file();
-    void xprintf(uint8 level, const char cszFormat[], ...);
+    void xprintf(uint8 level, const char *func, int line, const char cszFormat[], ...);
 protected:
     XLog();
     ~XLog();
     virtual void run() override;
+    virtual void on_timer() override;
+    bool open_new_file();
 private:
     LogParam param_;
     FILE * fd_;
-    uint32 lines_;
+    uint32 file_size_;
+    uint32 index_;
     Mutex *mtx_;
     std::string data_;
 };
 
 
-#define XCRASH(format, ...)	   XLog::instance()->xprintf(XLOG_CRASH, "[%s, %d] "##format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define XERROR(format, ...)    XLog::instance()->xprintf(XLOG_ERR, "[%s, %d] "##format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define XWARNING(format, ...)  XLog::instance()->xprintf(XLOG_WARNING, "[%s, %d] "##format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define XNOTICE(format, ...)   XLog::instance()->xprintf(XLOG_NOTICE, "[%s, %d] "##format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define XINFO(format, ...)     XLog::instance()->xprintf(XLOG_INFO, "[%s, %d] "##format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define XDEBUG(format, ...)    XLog::instance()->xprintf(XLOG_DEBUG, "[%s, %d] "##format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define XFATAL(format, ...)	   XLog::instance()->xprintf(LOG_FATAL, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define XERROR(format, ...)    XLog::instance()->xprintf(LOG_ERROR, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define XWARN(format, ...)     XLog::instance()->xprintf(LOG_WARN, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define XINFO(format, ...)     XLog::instance()->xprintf(LOG_INFO, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define XDEBUG(format, ...)    XLog::instance()->xprintf(LOG_DEBUG, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
 
-#define XOUT(format, ...) XLog::instance()->xprintf(XLOG_DEBUG, format, ##__VA_ARGS__)
 
 #endif // CORE_LOG_H
